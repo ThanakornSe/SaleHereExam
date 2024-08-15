1b2a7d6d65f161ff311f488dacd787ca67d1551f
Thanakorns <huang.thanakorn@gmail.com>
2024-08-15 10:49:06 +0700

    remove unused code

--- app/src/main/java/com/salehere/salehereexam/ui/home/compose/HomeMainScreen.kt
+++ app/src/main/java/com/salehere/salehereexam/ui/home/compose/HomeMainScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,8 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import com.salehere.salehereexam.ui.home.model.HomeUiState
-import com.salehere.salehereexam.ui.home.viewmodel.HomeViewModel
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun HomeMainScreen(
