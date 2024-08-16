16d16033aaa14007b6110542ea1b4f85f641f6f2
Thanakorns <huang.thanakorn@gmail.com>
2024-08-16 16:28:07 +0700

    add di to goal screen

--- app/src/main/java/com/salehere/salehereexam/di/AppModule.kt
+++ app/src/main/java/com/salehere/salehereexam/di/AppModule.kt
@@ -1,8 +1,10 @@
 package com.salehere.salehereexam.di
 
+import com.salehere.salehereexam.ui.goal.di.goalFeatureModule
 import com.salehere.salehereexam.ui.home.di.homeFeatureModule
 import org.koin.dsl.module
 
 val appModule = module {
     includes(homeFeatureModule)
+    includes(goalFeatureModule)
 }
--- app/src/main/java/com/salehere/salehereexam/ui/goal/di/GoalFeatureModule.kt
+++ app/src/main/java/com/salehere/salehereexam/ui/goal/di/GoalFeatureModule.kt
@@ -1,10 +1,13 @@
 package com.salehere.salehereexam.ui.goal.di
 
+import com.salehere.salehereexam.data.goal.di.goalDataModule
+import com.salehere.salehereexam.domain.goal.di.goalDomainModule
 import com.salehere.salehereexam.ui.goal.viewmodel.GoalViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val goalFeatureModule = module {
-
-    viewModel { GoalViewModel() }
+    includes(goalDataModule)
+    includes(goalDomainModule)
+    viewModel { GoalViewModel(get(),get()) }
 }
